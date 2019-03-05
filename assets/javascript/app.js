64a2d0d1126a90addcabf158795424d321f8d22b
Daniel Vadil <dvadz@yahoo.com>
2019-03-05 13:42:24 -0500

    got something that works, able to query any page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,58 +7,184 @@ var movieSearch = {
     ,t: ""
     ,type: "movie"
     ,y: ""
-    ,plot: ""
+    ,plot: "full"
     ,s: ""                              //THIS IS THE CURRENT USAGE
-    ,page: ""
+    ,page: "1"
     ,website: "https://www.omdbapi.com/?"
-    ,apikey: "apikey=trilogy"
+    ,apikey: "trilogy"
     ,url: "" 
-    ,response: {}
-    ,initResponse: function() {
-        this.response = {};
+    ,responses: []  //TODO: WARNING 'page' is used as the index, 'page' starts at 1 and so [0] will be unused
+    ,totalNumberOfPages: 0
+    ,initSearch: function() {
+        if(debug){console.log("initSearch")}
+        this.responses = [];
+        this.page = 1;
+        this.totalNumberOfPages = 0;
     }
-    ,saveSearchString(s) {
+    ,saveSearchParameters(s) {
         this.s = s;
     }
+    ,createTheQueryURL: function(){
+        this.url = this.website + "s=" + this.s + "&type=" + this.type + "&page=" + this.page + "&apikey=" + this.apikey;
+        if(debug){console.log(this.url)}
+    }
+    ,get queryURL(){
+        if(debug){console.log("get url: ", this.url)}
+        return this.url;
+    }
+    ,get pageNumber() {
+        if(debug){console.log("get page: ", this.page )}
+        return this.page;
+    }
+    ,set pageNumber(pageNumber){
+        if(debug){console.log("set page: ", this.page)}
+        this.page = pageNumber;
+    }
+    ,getAPageOfResults: function(){
+        return this.responses[this.page];
+    }
+
 };
 
-
+// SETUP EVENTS ----------------------------------------------
 $(document).ready(function(){
 
     if(debug){console.log("document is ready")}
     $("#btn-search").on("click", function(){
         event.preventDefault();
         if(debug){console.log("Event: clicked on 'Search'");}        
-        movieSearch.saveSearchString(s) = $("#input-search").val();
-        if(debug){console.log("Search: ", movieSearch.searchString);}        
-        startANewMovieSearchOnOMBD();
-       
+        
+        //if Search box is empty and user clicks SEARCH or presses ENTER, do nothing
+        if($("#input-search").val()==="") {
+            if(debug){console.log("No search string, terminating...");}        
+            return false;
+        
+        //Start the search and provide the search parameters
+        } else {
+            startANewSearchOnOMDB($("#input-search").val());
+        }
+    });
+
+    $(".div-pages").on("click", function(event){
+        if(debug){console.log("Event: clicked on a page button")}       
+    
+        // TWO conditions required in order to take action:
+        // 1. user clicked on a page button
+        // 2. page is NOT the current page
+        if(event.target.className === "pages" && movieSearch.page != event.target.value){
+            var page = event.target.value;
+            console.log("Page requested: ", page);
+            getAnotherPage(page);
+        } else {
+            if(debug) {
+                if(event.target.className != "pages"){
+                    console.log("You didn't click on a page button");
+                } else {
+                    console.log("You are already on that page");
+                }
+            }
+        }
+
     });
 });
 
-function startANewMovieSearchOnOMBD(){
-    if(debug){console.log("function: performMovieSearchOnOMDB");}
-    var queryURL = "https://www.omdbapi.com/?s=" + movieSearch.s + "&type=movie&y=&page=1&apikey=trilogy";
+// START A SEARCH --------------------------------------------
 
-    $.ajax({ 
-        url: queryURL, 
-        method: "GET"
-    }).then(function(response){ 
-        movieSearch.response.push();
-        parse();
-    });
+function startANewSearchOnOMDB(){
+    if(debug){console.log("function: startANewSearchOnOMDB");}
+   
+    //clear all previous search parameters and results
+    movieSearch.initSearch();
+    //empty the previous search from the screen
+    initDisplay()
+    saveAllParameters();
+    movieSearch.createTheQueryURL();
+    sendAjaxQuery(movieSearch.queryURL);
+} 
+
+function sendAjaxQuery(url){
+    if(debug){console.log("function: sendAjaxQuery")}
+
+    //check whether the page is in the 'movieSearch.responses' array
+    if(movieSearch.responses[movieSearch.page]===undefined){
+        if(debug){console.log("function: sendAjaxQuery - sending ajax request")}
+        $.ajax({ 
+            url: url, 
+            method: "GET"
+        }).then(function(response){ 
+            if(debug){console.log("Ajax response", response);}
+            processQueryResults(response);
+        });
+    } else {
+        if(debug){console.log("function: sendAjaxQuery - NOOooo ajax request")}
+        var onePage = movieSearch.getAPageOfResults();
+        //Proceed to display results
+        displayAPage(onePage);      
+    }
+
 }
 
-function buildTheURLString(){
+function processQueryResults(response){
+    if(debug) {console.log("function: processQueryResults", response)}
+
+    //SAVE query results and check return status, if FALSE then show a failed message
+    var status = saveResponse(response);
+
+    //if TRUE, query was successful
+    if(status) {
+        //grab a page of results
+        var onePage = movieSearch.getAPageOfResults();
+        //Proceed to display results
+        displayAPage(onePage);
+    } else {
+        displayFailed()
+    }
+}
+
+ 
+function saveResponse(response){
+    if(debug){console.log("function: saveResponse: ", response)}
+
+    //IF Successful
+    if(response.Response) {
+        movieSearch.responses[movieSearch.page] = response.Search;
+        console.log(movieSearch.responses);
+        //calculate number of pages
+        movieSearch.totalNumberOfPages = Math.ceil((response.totalResults/10))
+        //display butons for pages
+        createPageButtons();
+        return true;
+    //QUERY HAD AN ERROR FOR WHATEVER REASON
+    } else {
+        return false;
+    }
 
 }
 
-function parse(){
-    if(debug){console.log("function: parse ")}
+function saveAllParameters(){
+   //save all arguments provided by user
+   movieSearch.saveSearchParameters($("#input-search").val());
+   if(debug){console.log("Search: ", movieSearch.s);}        
+}
 
-    var movies = movieSearch.response.Search;
-    console.log(movies)
-    movies.forEach(function(aMovie){
+function getAnotherPage(page) {
+    clearResults();
+    movieSearch.pageNumber = page;
+    movieSearch.createTheQueryURL();
+    sendAjaxQuery(movieSearch.queryURL);
+}
+
+// ALL ABOUT DISPLAYING -------------------------------------------------
+
+function displayFailed() {
+    if(debug){console.log("query failed. try again.")}
+    //TODO: inform the user of the error
+}
+
+function displayAPage(onePage){
+    if(debug){console.log("function: displayAPage ", onePage)}
+    onePage.forEach(function(aMovie){
+        if(debug){console.log(aMovie)}
         var card = createACard(aMovie);
         appendCardToResults(card);
     });
@@ -81,4 +207,24 @@ function createACard(aMovie){
 
 function appendCardToResults(card) {
     $("div.results").append(card);
+}
+
+function initDisplay() {
+    //empty out the div
+    clearResults();
+    //hide the page buttons
+}
+
+function clearResults() {
+    $(".results").empty();
+}
+
+// PAGE BUTTONS ------------------------------------------
+function createPageButtons(){
+    var numberOfPages  = movieSearch.totalNumberOfPages;
+    if(debug){console.log("function: createPageButtons ", numberOfPages)}
+    for( var i = 1; i < numberOfPages+1; i++) {
+        var newButton = $("<button></button>").text(i).attr("value", i).addClass("pages");
+        $(".div-pages").append(newButton);
+    }
 }
